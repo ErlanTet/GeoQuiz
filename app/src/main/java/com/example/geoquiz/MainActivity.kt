79ec8bc4be41a346e31a162dae73ee6d1723ff13
ealmaz <ealmaz@nurtelecom.kg>
2021-11-03 18:04:54 +0600

    chapter 4

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,108 +1,75 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 
-private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
+
+    private val vm: QuestionViewModel by lazy {
+        ViewModelProviders.of(this).get(QuestionViewModel::class.java)
+    }
 
     private lateinit var btnTrue: Button
     private lateinit var btnFalse: Button
     private lateinit var btnNext: ImageButton
     private lateinit var btnPrev: ImageButton
     private lateinit var tvQuestion: TextView
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
-
-    private var score = 0
-
-    private var currentIndex = 0
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        vm.currentIndex = currentIndex
+
         btnTrue = findViewById(R.id.btn_true)
         btnFalse = findViewById(R.id.btn_false)
         btnNext = findViewById(R.id.btn_next)
         btnPrev = findViewById(R.id.btn_prev)
         tvQuestion = findViewById(R.id.tv_question)
 
-        tvQuestion.setOnClickListener { nextQuestion() }
+        tvQuestion.setOnClickListener { vm.moveToNext() }
         btnTrue.setOnClickListener { checkAnswer(true) }
         btnFalse.setOnClickListener { checkAnswer(false) }
-        btnNext.setOnClickListener { nextQuestion() }
-        btnPrev.setOnClickListener { prevQuestion() }
-        updateQuestion()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    private fun nextQuestion() {
-        currentIndex++
-        if (currentIndex == questionBank.size) {
-            showResult()
-            currentIndex = 0
-            return
+        btnNext.setOnClickListener {
+            vm.moveToNext()
+            updateQuestion()
+        }
+        btnPrev.setOnClickListener {
+            vm.moveToPrev()
+            updateQuestion()
         }
         updateQuestion()
     }
 
-    private fun prevQuestion() {
-        if (currentIndex == 0) currentIndex = questionBank.size
-        currentIndex--
-        updateQuestion()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        System.out.println("OnSaveInstanceState")
+        outState.putInt(KEY_INDEX, vm.currentIndex)
     }
 
     private fun updateQuestion() {
-        val questionResId = questionBank[currentIndex].textResId
+        val questionResId = vm.currentQuestionTextId
         tvQuestion.setText(questionResId)
         isButtonsEnabled(true)
     }
 
     private fun checkAnswer(answer: Boolean) {
         isButtonsEnabled(false)
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = vm.currentQuestionAnswer
         val messageResId = if (answer == correctAnswer) {
-            score++
             R.string.correct
-        } else { R.string.incorrect }
+        } else {
+            R.string.incorrect
+        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 
@@ -111,9 +78,18 @@ class MainActivity : AppCompatActivity() {
         btnFalse.isEnabled = value
     }
 
-    private fun showResult() {
-        val percent = 100 / questionBank.size * score
-        val text = getString(R.string.you_result_is, percent)
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    override fun onPause() {
+        super.onPause()
+        System.out.println("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        System.out.println("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        System.out.println("onDestroy")
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/QuestionViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.geoquiz
+
+import androidx.lifecycle.ViewModel
+
+class QuestionViewModel : ViewModel() {
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    var currentIndex = 0
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionTextId: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrev() {
+        if (currentIndex == 0) currentIndex = questionBank.size
+        currentIndex--
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        System.out.println("onCleared")
+    }
+}
